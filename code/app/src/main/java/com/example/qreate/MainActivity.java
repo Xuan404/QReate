3604ad698f59962d75493935d815642f800f7140
Shraddha-Mehta <149219530+Shraddha-Mehta@users.noreply.github.com>
2024-03-02 05:37:51 -0700

    Updated edit_profile_info.xml

---
+++ code/app/src/main/java/com/example/qreate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.qreate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_management_screen);
+    }
+}
