dd30dac243f5e6e893c329f0e5e77f86ea962c9c
Akib424 <145165683+Akib424@users.noreply.github.com>
2024-03-02 00:25:45 -0800

    Created QR-code menu
    Hopefully shit doesn't crash

--- code/app/src/main/java/com/example/qreate/MainActivity.java
+++ code/app/src/main/java/com/example/qreate/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.generate_qr_screen);
+        setContentView(R.layout.qr_menu_screen);
     }
 }
