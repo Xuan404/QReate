3b7e117e0ebbbf7b40863f68a5aab207456add0e
Akib424 <145165683+Akib424@users.noreply.github.com>
2024-03-04 06:22:45 -0800

    Set up the navigation bar for other Fragments
    now you can switch between fragments in navigation bar yayy

--- code/app/src/main/java/com/example/qreate/OrganizerActivity.java
+++ code/app/src/main/java/com/example/qreate/OrganizerActivity.java
@@ -1,11 +1,15 @@
 package com.example.qreate;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class OrganizerActivity extends AppCompatActivity {
     @Override
@@ -14,15 +18,64 @@ public class OrganizerActivity extends AppCompatActivity {
         setContentView(R.layout.organizer_handler);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.organizer_handler_navigation_bar);
-        bottomNavigationView.setSelectedItemId(R.id.defaultNavPlaceholder);
-        //bottomNavigationView.setVisibility(View.INVISIBLE);
+        bottomNavigationView.setSelectedItemId(R.id.defaultNavPlaceholder); //This line is here so that there is no default item selected, it selects a menu item that is invisible
 
-
-        //I AM STILL TESTING OUT HOW THIS WORKS
+        //inflates the homescreen fragment automatically
         HomeScreenFragment homeScreenFragment = new HomeScreenFragment();
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.organizer_handler_frame,homeScreenFragment).commit();
+
+        //Used if/else to check for selected id because switch is being a bitch
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.qr_menu) {
+                    selectedFragment = new QRmenuFragment();
+                } else if (itemId == R.id.notifications_menu) {
+                    selectedFragment = new NotificationsMenuFragment();
+                } else if (itemId == R.id.attendee_list_menu) {
+                    selectedFragment = new AttendeeListMenuFragment();
+                } else if (itemId == R.id.geolocation_menu) {
+                    selectedFragment = new GeolocationMenuFragment();
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.organizer_handler_frame, selectedFragment).commit();
+                    return true;
+                }
+                return true;
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        bottomNavigationView.setSelectedItemId(R.id.defaultNavPlaceholder);
+//        //bottomNavigationView.setVisibility(View.INVISIBLE);
+//
+//
+//        //I AM STILL TESTING OUT HOW THIS WORKS
+//        HomeScreenFragment homeScreenFragment = new HomeScreenFragment();
+//        getSupportFragmentManager().beginTransaction()
+//                .add(R.id.organizer_handler_frame,homeScreenFragment).commit();
     }
+
+
 
 
 }
