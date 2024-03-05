184c7379757b1cca022b5e7b7960f6ea85ed42b6
Gitanjali Ghugare <gghugare09@gmail.com>
2024-03-05 01:47:28 -0700

    set up Administrator home screen with navigation bar

--- code/app/src/main/java/com/example/qreate/MainActivity.java
+++ code/app/src/main/java/com/example/qreate/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.qreate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.qreate.administrator.AdministratorActivity;
 import com.example.qreate.organizer.OrganizerActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
         administratorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, AdministratorActivity.class);
+                startActivity(intent);
             }
         });
 
