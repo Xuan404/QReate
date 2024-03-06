35e9aa07463c0d493e60f460f2dfa43419e552c7
Shraddha-Mehta <149219530+Shraddha-Mehta@users.noreply.github.com>
2024-03-05 22:36:14 -0700

    Createt nav bar, scan page for attendee and other related changes

---
+++ code/app/src/main/java/com/example/qreate/attendee/AttendeeActivity.java
@@ -0,0 +1,29 @@
+package com.example.qreate.attendee;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.qreate.R;
+import com.example.qreate.organizer.HomeScreenFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AttendeeActivity extends AppCompatActivity {
+
+    /*
+     This class is used as a MainActivity class for the Attendee UI
+     */
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.attendee_tap_to_scan_qr);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.attendee_navigation_bar);
+        bottomNavigationView.setSelectedItemId(R.id.defaultNavPlaceholder); //This line is here so that there is no default item selected, it selects a menu item that is invisible
+
+        //inflates the homescreen fragment automatically
+        HomeScreenFragment homeScreenFragment = new HomeScreenFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.attendee_nav_frame,homeScreenFragment).commit();
+    }
+}
