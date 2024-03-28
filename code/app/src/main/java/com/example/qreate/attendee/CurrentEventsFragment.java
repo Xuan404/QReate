3c6d9980cc79a109628064c2687397dd305512ac
Shraddha-Mehta <149219530+Shraddha-Mehta@users.noreply.github.com>
2024-03-27 22:59:35 -0600

    added drop down menus for attendee events page

--- code/app/src/main/java/com/example/qreate/attendee/CurrentEventsFragment.java
+++ code/app/src/main/java/com/example/qreate/attendee/CurrentEventsFragment.java
@@ -2,15 +2,24 @@ package com.example.qreate.attendee;
 
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.qreate.AccountProfileScreenFragment;
 import com.example.qreate.R;
 
 public class CurrentEventsFragment extends Fragment {
@@ -19,7 +28,13 @@ public class CurrentEventsFragment extends Fragment {
     public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.current_events_listview,container,false);
         AppCompatButton backButton = view.findViewById(R.id.button_back_current_event_details);
-
+        ImageButton profileButton = view.findViewById(R.id.current_events_profic_pic_icon);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view);
+            }
+        });
         backButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -34,6 +49,49 @@ public class CurrentEventsFragment extends Fragment {
     private void goBackToAttendeeEventDetails(){
         getParentFragmentManager().popBackStack();
     }
+    private void showPopupMenu(View view) {
+        // Initialize the PopupMenu
+        PopupMenu popupMenu = new PopupMenu(getActivity(), view); // For Fragment, use getActivity() instead of this
+        popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
 
+        //Sets text color to white
+        for (int i = 0; i < popupMenu.getMenu().size(); i++) {
+            MenuItem item = popupMenu.getMenu().getItem(i);
+            SpannableString spanString = new SpannableString(popupMenu.getMenu().getItem(i).getTitle().toString());
+            spanString.setSpan(new ForegroundColorSpan(Color.WHITE), 0, spanString.length(), 0); // Set color to white
+            item.setTitle(spanString);
+        }
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.profile_account) {
+                    accountProfile();
+                    return true;
+
+                } else if (id == R.id.profile_logout) {
+                    getActivity().finish();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void accountProfile() {
+        //Handles fragment transaction related to the account profile
+
+        ((AttendeeActivity)getActivity()).hideBottomNavigationBar();
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.attendee_handler_frame, new AccountProfileScreenFragment("attendee"));
+        transaction.addToBackStack(null); // Add this transaction to the back stack
+        transaction.commit();
+    }
 
 }
--- code/app/src/main/java/com/example/qreate/attendee/UpcomingEventsFragment.java
+++ code/app/src/main/java/com/example/qreate/attendee/UpcomingEventsFragment.java
@@ -2,15 +2,24 @@ package com.example.qreate.attendee;
 
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.qreate.AccountProfileScreenFragment;
 import com.example.qreate.R;
 
 public class UpcomingEventsFragment extends Fragment {
@@ -21,7 +30,13 @@ public class UpcomingEventsFragment extends Fragment {
         View view = inflater.inflate(R.layout.upcoming_events_listview,container,false);
 
         AppCompatButton backButton = view.findViewById(R.id.button_back_upcoming_event_details);
-
+        ImageButton profileButton = view.findViewById(R.id.upcoming_events_profile_pic_icon);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view);
+            }
+        });
         backButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -35,6 +50,50 @@ public class UpcomingEventsFragment extends Fragment {
 
     private void goBackToAttendeeEventDetails(){
         getParentFragmentManager().popBackStack();
+    }
+    private void showPopupMenu(View view) {
+        // Initialize the PopupMenu
+        PopupMenu popupMenu = new PopupMenu(getActivity(), view); // For Fragment, use getActivity() instead of this
+        popupMenu.getMenuInflater().inflate(R.menu.profile_menu, popupMenu.getMenu());
+
+        //Sets text color to white
+        for (int i = 0; i < popupMenu.getMenu().size(); i++) {
+            MenuItem item = popupMenu.getMenu().getItem(i);
+            SpannableString spanString = new SpannableString(popupMenu.getMenu().getItem(i).getTitle().toString());
+            spanString.setSpan(new ForegroundColorSpan(Color.WHITE), 0, spanString.length(), 0); // Set color to white
+            item.setTitle(spanString);
+        }
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.profile_account) {
+                    accountProfile();
+                    return true;
+
+                } else if (id == R.id.profile_logout) {
+                    getActivity().finish();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void accountProfile() {
+        //Handles fragment transaction related to the account profile
+
+        ((AttendeeActivity)getActivity()).hideBottomNavigationBar();
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.attendee_handler_frame, new AccountProfileScreenFragment("attendee"));
+        transaction.addToBackStack(null); // Add this transaction to the back stack
+        transaction.commit();
     }
 
 
