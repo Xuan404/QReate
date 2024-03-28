5b9109e8e9d6e28a7ca24fae16adfab90ceb0433
redtrovelvet <harshita.h2810@gmail.com>
2024-03-28 06:25:13 -0600

    view event details for attendee

--- code/app/src/main/java/com/example/qreate/attendee/AttendeeEventViewDetailsFragment.java
+++ code/app/src/main/java/com/example/qreate/attendee/AttendeeEventViewDetailsFragment.java
@@ -1,6 +1,43 @@
 package com.example.qreate.attendee;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.qreate.R;
+
+import org.w3c.dom.Text;
+
 public class AttendeeEventViewDetailsFragment extends Fragment {
+    private ImageView poster;
+    private TextView eventName;
+    private TextView eventOrganizer;
+    private TextView eventDescription;
+    private TextView eventDate;
+    private TextView eventTime;
+    private TextView eventLocation;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.attendee_event_view_details, container, false);
+
+        poster = view.findViewById(R.id.event_details_poster);
+        eventName = view.findViewById(R.id.event_details_name);
+        eventOrganizer = view.findViewById(R.id.event_details_organizer);
+        eventDescription = view.findViewById(R.id.event_details_description);
+        eventDate = view.findViewById(R.id.event_details_date);
+        eventTime = view.findViewById(R.id.event_details_time);
+        eventLocation = view.findViewById(R.id.event_details_location);
+
+        return view;
+
+    }
 }
