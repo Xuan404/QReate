5094f645e6e0c804d8e97b709e0c863497304523
Akib424 <145165683+Akib424@users.noreply.github.com>
2024-04-06 03:05:29 -0600

    created the xml files for org_attendee list view and changed the font for admin xmls

--- code/app/src/main/java/com/example/qreate/organizer/attendeesmenu/OrganizerAttendeeCheckinListActivity.java
+++ code/app/src/main/java/com/example/qreate/organizer/attendeesmenu/OrganizerAttendeeCheckinListActivity.java
@@ -7,14 +7,20 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.qreate.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class OrganizerAttendeeCheckinListActivity extends AppCompatActivity {
+
+    private String eventDocId = "5kkFuEgp2jycjREHiq14"; // Dummy data
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.organizer_attendee_list_attendee_checkins);
+
+        db = FirebaseFirestore.getInstance();
 
         ImageButton backButton = findViewById(R.id.attendee_checkin_list_screen_backbutton);
 
